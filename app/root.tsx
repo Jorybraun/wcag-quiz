72ec5e568abe921bc7dcef97ab1c9b314bc3e3d7
jory braun <jory.braun@sycle.net>
2022-02-01 00:25:27 -0500

    moves manifest to public

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="manifest" href="/manifest.webmanifest"/>
+        <link rel="manifest" href="public/manifest.webmanifest"/>
         <Meta />
         <Links />
       </head>
